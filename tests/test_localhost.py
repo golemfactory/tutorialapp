55fdaba040f4ad705300cf272bf5ce4c2f2c9c70
Marek Franciszkiewicz <marek@golem.network>
2019-11-18 11:10:32 +0100

    Cleanup the localhost test

--- tests/test_localhost.py
+++ tests/test_localhost.py
@@ -26,18 +26,3 @@ class TestLocalhost(SimulationBase):
             requestor_handler=RequestorHandler(),
             provider_handler=ProviderHandler(),
         )
-
-    @pytest.mark.asyncio
-    async def test_requestor_benchmark(self, task_lifecycle_util):
-        async with task_lifecycle_util.init_requestor(
-                self._get_task_api_service):
-            score = await task_lifecycle_util.requestor_client.run_benchmark()
-            assert score > 0
-
-    @pytest.mark.asyncio
-    async def test_provider_benchmark(self, task_lifecycle_util):
-        task_id = '0xf00f'
-        task_lifecycle_util.init_provider(self._get_task_api_service, task_id)
-        await task_lifecycle_util.start_provider()
-        score = await task_lifecycle_util.provider_client.run_benchmark()
-        assert score > 0
