f7797f2282588dac3c9a5b199ecf0529134f57d8
Marek Franciszkiewicz <marek@golem.network>
2019-11-28 01:35:57 +0100

    Task API 0.25.0

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -1,10 +1,10 @@
 # pylint: disable=unused-import, redefined-outer-name
+import shlex
+import subprocess
 from pathlib import Path
 
-import docker
-import pytest
-
 from golem_task_api import TaskApiService
+from golem_task_api.testutils.dockertaskapiservice import DockerTaskApiService
 
 from tutorial_app import constants
 
@@ -12,22 +12,17 @@ from .base import (
     SimulationBase,
     task_lifecycle_util,
 )
-from .docker import (
-    is_docker_available,
-    DockerTaskApiService,
-)
 
 
-@pytest.mark.skipif(not is_docker_available(), reason='docker not available')
 class TestDocker(SimulationBase):
     IMAGE = f"{constants.DOCKER_IMAGE}:test"
 
     @classmethod
     def setup_class(cls):
-        docker.from_env().images.build(
-            path=str(Path(__file__).parent.parent / 'image'),
-            tag=cls.IMAGE,
-        )
+        path = Path(__file__).parent.parent / 'image'
+        subprocess.check_call(shlex.split(
+            f'docker build -t {cls.IMAGE} -f {path / "Dockerfile"} {path}'
+        ))
 
     def _get_task_api_service(
             self,
